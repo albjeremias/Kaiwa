6bb2285f5432cb1fd2344081d217f5f5ca2fa61e
Friedrich von Never <friedrich@fornever.me>
2016-05-07 22:13:29 +0700

    Project: update TypeScript, migrate to typings

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../../typings/main.d.ts" />
 'use strict';
 
 global.Buffer = global.Buffer || require('buffer').Buffer;
--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="async/async.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="classnames/classnames.d.ts" />
