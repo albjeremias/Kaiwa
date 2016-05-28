c1703f7f4a43adbd110d9b6028d40a7f09f47ad7
Robert Schuh <github@eneticum.de>
2016-05-28 17:29:45 +0200

    add login, next up - app render method

--- src/js/views/login.tsx
+++ src/js/views/login.tsx
@@ -1,4 +1,5 @@
-import { Component, StatelessComponent } from 'react'
+import { browserHistory } from 'react-router';
+import { Component, StatelessComponent } from 'react';
 
 class Field extends Component<{
     type?: string;
@@ -8,6 +9,8 @@ class Field extends Component<{
     tabIndex?: number;
     autoFocus?: boolean;
     title?: string;
+    value?: any;
+    onChange?: any;
 }, {}> {
     static defaultProps = {
         id: '',
@@ -15,7 +18,7 @@ class Field extends Component<{
         autoFocus: false
     }
 
-    render() {
+    render () {
         const props = this.props;
         const isCheckBox = props.type === 'checkbox';
         const className = 'fieldContainer' + (isCheckBox ? ' checkbox' : '');
@@ -25,8 +28,10 @@ class Field extends Component<{
                              id={props.id}
                              placeholder={props.placeholder}
                              tabIndex={props.tabIndex}
-                             autoFocus={props.autoFocus} />;
-        const components = isCheckBox ?[ input, label ] : [ label, input ];
+                             autoFocus={props.autoFocus}
+                             value={props.value}
+                             onChange={props.onChange} />;
+        const components = isCheckBox ? [ input, label ] : [ label, input ];
 
         return (
             <div className={className} title={props.title}>
@@ -36,8 +41,50 @@ class Field extends Component<{
     }
 }
 
-export class Login extends Component<{}, {}> {
-    render() {
+export class Login extends Component<{}, {
+    jid?: string;
+    password?: string;
+    server?: string;
+    connURL?: string;
+    wsURL?: string;
+    boshURL?: string;
+    transport?: string;
+}> {
+
+    constructor () {
+        super();
+        this.state = {};
+    }
+
+    handleSubmit (event) {
+        event.preventDefault();
+
+        console.log(this, this.state);
+
+        if(window.SERVER_CONFIG.domain && this.state.jid.indexOf('@') == -1)
+            this.state.jid += "@" + window.SERVER_CONFIG.domain;
+
+        if(window.SERVER_CONFIG.wss)
+            this.state.connURL = window.SERVER_CONFIG.wss;
+
+        if (this.state.connURL.indexOf('http') === 0) {
+            this.state.boshURL = this.state.connURL;
+            this.state.transport = 'bosh';
+        } else if (this.state.connURL.indexOf('ws') === 0) {
+            this.state.wsURL = this.state.connURL;
+            this.state.transport = 'websocket';
+        }
+
+        localStorage.setItem("session", JSON.stringify(this.state));
+
+        browserHistory.push('/');
+    }
+
+    handleChange (event) {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
+    render () {
         const showWssSelector = !window.SERVER_CONFIG.wss;
         return (
             <section className="loginbox content box">
@@ -45,17 +92,34 @@ export class Login extends Component<{}, {}> {
                     <h2>Log in</h2>
                 </div>
                 <div className="content">
-                    <form id="login-form">
-                        <Field id="jid" label="Username" placeholder="you" tabIndex={1} autoFocus={true} />
+                    <form id="login-form" onSubmit={e => this.handleSubmit(e)}>
+                        <Field id="jid"
+                               label="Username"
+                               placeholder="you"
+                               tabIndex={1}
+                               autoFocus={true}
+                               value={this.state.jid}
+                               onChange={e => this.handleChange(e)} />
                         <Field id="password"
                                type="password"
                                label="Password"
                                placeholder="••••••••"
-                               tabIndex={2} />
+                               tabIndex={2}
+                               value ={this.state.password}
+                               onChange={e => this.handleChange(e)} />
                         {showWssSelector
-                            ? <Field id="connURL" label="WebSocket or BOSH URL" placeholder="wss://aweso.me:5281/xmpp-websocket" tabIndex={3} />
+                            ? <Field id="connURL"
+                                     label="WebSocket or BOSH URL"
+                                     placeholder="wss://aweso.me:5281/xmpp-websocket"
+                                     tabIndex={3}
+                                     value={this.state.connURL}
+                                     onChange={e => this.handleChange(e)} />
                             : null}
-                        <Field id="public-computer" label="Public computer" type="checkbox" tabIndex={4} title="Do not remember password" />
+                        <Field id="public-computer"
+                               label="Public computer"
+                               type="checkbox"
+                               tabIndex={4}
+                               title="Do not remember password" />
 
                         <button type="submit" tabIndex={5} className="primary">Go!</button>
                     </form>
@@ -63,6 +127,7 @@ export class Login extends Component<{}, {}> {
             </section>
         );
     }
+
 }
 
 /* TODO:
